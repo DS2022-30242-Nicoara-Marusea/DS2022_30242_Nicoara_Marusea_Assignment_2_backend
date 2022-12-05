f3ef903cb271910796fe294aca21bd20fa2b6bdc
Marusea Ioana Nicoară <36156196+maelves@users.noreply.github.com>
2022-12-05 16:37:32 +0200

    test 1 for CI/CD

---
+++ src/main/java/ro/tuc/ds2020/dtos/DeviceDTO.java
@@ -0,0 +1,25 @@
+package ro.tuc.ds2020.dtos;
+
+import lombok.*;
+import ro.tuc.ds2020.entities.Sensor;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeviceDTO {
+    private UUID id;
+
+    private String description;
+
+    private String address;
+
+    private double max_hourly_energy_consumption;
+
+    private double average_consumption;
+
+    private UUID deviceUser;
+}
